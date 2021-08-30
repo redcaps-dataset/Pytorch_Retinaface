93b8717e37e1222a6d339b702ce1949e0bf4054b
Karan Desai <kddeskar@gmail.com>
2021-08-30 15:05:43 -0400

    Update hubconf with direct download link.

--- hubconf.py
+++ hubconf.py
@@ -13,13 +13,17 @@ CFG_RESNET50 = {
     "out_channel": 256
 }
 
+RESNET50_URL = "https://www.dropbox.com/s/wlaey1rk8id2i5t/retinaface_resnet50.pth?dl=1"
 
-def retinaface_resnet50(landmark_head: bool = True, **kwargs):
+
+def retinaface_resnet50(pretrained=True, **kwargs):
 
     model = RetinaFace(cfg=CFG_RESNET50, phase="test")
     model.eval()
 
-    if not landmark_head:
-        model.LandmarkHead = torch.nn.Identity()
+    if pretrained:
+        model.load_state_dict(
+            torch.hub.load_state_dict_from_url(RESNET50_URL, progress=False)
+        )
 
     return model
